f2a9feba2eeb17a969f7f9ce33ac5558b45d5960
a0920005158 <a0920005158@gmail.com>
2021-10-04 17:26:20 +0800

    no message

--- test2.py
+++ test2.py
@@ -1,13 +1,43 @@
 import pyautogui
+import keyboard
+import time
 import PIL
+import time
 from PIL import Image
 
+def error_msg():
+    print('error');
+    time.sleep(2)
+    start_game_process()
+
+def start_game_process():
+    try:
+        picturePos = pyautogui.locateOnScreen('test.png')  #  檔案名稱依自己設置處理
+        if picturePos == None:
+            error_msg();
+            return;
+        print(picturePos[0]);
+        pictureMidPos = pyautogui.center((picturePos[0], picturePos[1], picturePos[2], picturePos[3]))  #  取得該圖像的中間值(1519，146)
+        print(pictureMidPos);
+        pyautogui.click((pictureMidPos[0], pictureMidPos[1]))  #  點擊p
+        start_game_process()
+    except:
+        error_msg();
+        return;
+    
+while True:
+    if keyboard.read_key() == "f6":
+        start_game_process()
+        break
+    if keyboard.read_key() == "f7":
+        print('f777777777777777777!');
+        break
+
+# https://medium.com/pyladies-taiwan/python-%E5%B0%87python%E6%89%93%E5%8C%85%E6%88%90exe%E6%AA%94-32a4bacbe351
+# https://martychen920.blogspot.com/2017/06/python-pyautogui_20.html
 # 紀錄滑鼠座標
 # pyautogui.displayMousePosition() 
 # 單擊畫面
-pyautogui.click(x=100,y=150,button='left')
+# pyautogui.click(x=100,y=150,button='left')
 # 雙擊畫面
 # pyautogui.doubleClick(x=100,y=150,button="left")
-# pyautogui.locateOnScreen('test.png')  #  檔案名稱依自己設置處理
-# pyautogui.center((1487, 133, 65, 26))  #  取得該圖像的中間值(1519，146)
-# pyautogui.click((1519, 146))  #  點擊
